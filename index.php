b5845db9c90e95a56036c4506309f3125be46bbe
Michel Frechette <mifrec@hotmail.com>
2018-04-11 08:08:19 -0400

    3-8-G introduction to looping

--- index.php
+++ index.php
@@ -3,8 +3,3 @@
   $names = array('michel', 'carole', 'pauline', 'julie');
 
 ?>
-
-<p>Hi, my name is <?php echo $names[0]; ?></p>
-<p>Hi, my name is <?php echo $names[1]; ?></p>
-<p>Hi, my name is <?php echo $names[2]; ?></p>
-<p>Hi, my name is <?php echo $names[3]; ?></p>
