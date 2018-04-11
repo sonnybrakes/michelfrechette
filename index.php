57895cbe6902895fbdc81f69fe256a270c8275f4
Michel Frechette <mifrec@hotmail.com>
2018-04-11 14:50:08 -0400

    4-9-J add the_permalink() wp-function inside of <a> tags to link each wordpress post on the list to each individual post

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
   while(have_posts()) {
     the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php the_content(); ?>
         <hr>
   <?php }
