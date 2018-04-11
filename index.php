cdce080e40977f4df6133d543f68fbd6d8277a7d
Michel Frechette <mifrec@hotmail.com>
2018-04-11 07:54:22 -0400

    3-8-F one for each of the names in the names array

--- index.php
+++ index.php
@@ -5,3 +5,6 @@
 ?>
 
 <p>Hi, my name is <?php echo $names[0]; ?></p>
+<p>Hi, my name is <?php echo $names[1]; ?></p>
+<p>Hi, my name is <?php echo $names[2]; ?></p>
+<p>Hi, my name is <?php echo $names[3]; ?></p>
