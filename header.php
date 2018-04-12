f9d40a9f189c589921850fb0f742a1a8dbd8bff5
Michel Frechette <mifrec@hotmail.com>
2018-04-12 19:41:24 -0400

    5-13-U delete the # placeholder hashtags from some of the href <a> tags in footer.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
   <body>
     <header class="site-header">
       <div class="container">
-        <h1 class="school-logo-text float-left"><a href="#"><strong>Fictional</strong> University</a></h1>
+        <h1 class="school-logo-text float-left"><a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a></h1>
         <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
