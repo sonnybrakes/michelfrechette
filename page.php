fcb7532ff1e9611893007067716292fc94948b45
Michel Frechette <mifrec@hotmail.com>
2018-04-12 18:47:31 -0400

    5-13-J delete images/ocean.jpg from the url(images/ocean.jpg);

--- page.php
+++ page.php
@@ -4,7 +4,7 @@
     the_post(); ?>
 
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg);"></div>
+      <div class="page-banner__bg-image" style="background-image: url();"></div>
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
