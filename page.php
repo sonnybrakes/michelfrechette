5df735bfce65bb767e3dafd60b3d879167a39a6d
Michel Frechette <mifrec@hotmail.com>
2018-04-12 17:30:21 -0400

    5-13-I write a note in page.php 'DON'T FORGET TO REPLACE ME LATER'

--- page.php
+++ page.php
@@ -8,7 +8,7 @@
       <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+          <p>DON'T FORGET TO REPLACE ME LATER</p>
         </div>
       </div>
     </div>
@@ -28,7 +28,7 @@
         </ul>
       </div>
       -->
-      
+
       <div class="generic-content">
         <?php the_content(); ?>
       </div>
