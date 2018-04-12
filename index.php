9d266d73bd4bda278de4711b90a7a955828c954b
Michel Frechette <mifrec@hotmail.com>
2018-04-12 08:32:50 -0400

    4-11-M delete the while loop from index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php get_header();
 
-  while(have_posts()) {
-    the_post(); ?>
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-        <?php the_content(); ?>
-        <hr>
-  <?php }
 
   get_footer();
 
