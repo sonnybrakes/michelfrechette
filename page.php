20e86cb422c8201fcb8dfc17bf5bcbafe3192ca4
Michel Frechette <mifrec@hotmail.com>
2018-04-12 17:14:37 -0400

    5-13-E inside the php tags where 'Our History' was call the wp-function the_title(); in page.php

--- page.php
+++ page.php
@@ -6,7 +6,7 @@
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg);"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php  ?></h1>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
           <p>Learn how the school of your dreams got started.</p>
         </div>
