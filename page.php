3b7cdd5a5836740a6909e3eb904112940951267a
Michel Frechette <mifrec@hotmail.com>
2018-04-13 07:19:41 -0400

    5-14-B drop into php where we will use an if statement

--- page.php
+++ page.php
@@ -15,6 +15,8 @@
 
     <div class="container container--narrow page-section">
 
+      <?php  ?>
+      
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
       </div>
