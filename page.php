fe3ddd1b25de421b0e7424f93fbf1274c08520db
Michel Frechette <mifrec@hotmail.com>
2018-04-13 09:06:29 -0400

    5-14-N delete/remove placeholder text from inside the 'if statement'

--- page.php
+++ page.php
@@ -17,7 +17,6 @@
 
       <?php
         if (wp_get_post_parent_id(get_the_ID())) { ?>
-          "WE WILL DROP IN AND OUT OF PHP AND HTML INSIDE THIS IF STATEMENT"
 
           <div class="metabox metabox--position-up metabox--with-home-link">
             <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
