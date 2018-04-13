01685240c59caf428b839ac8d0f82afb176d9c75
Michel Frechette <mifrec@hotmail.com>
2018-04-13 07:21:40 -0400

    5-14-C create the basic structure of an 'if statement'

--- page.php
+++ page.php
@@ -15,8 +15,12 @@
 
     <div class="container container--narrow page-section">
 
-      <?php  ?>
-      
+      <?php
+        if () {
+          
+        }
+      ?>
+
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
       </div>
