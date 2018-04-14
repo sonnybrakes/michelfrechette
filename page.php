ed1a138b1538f46998f399fbe0e89dafa036d564
Michel Frechette <mifrec@hotmail.com>
2018-04-14 08:37:07 -0400

    5-16-B delete hard coded list items for the side menu

--- page.php
+++ page.php
@@ -29,11 +29,10 @@
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+      
         </ul>
       </div>
-      
+
 
       <div class="generic-content">
         <?php the_content(); ?>
