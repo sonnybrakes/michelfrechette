58cf89e0d6c1117504852e06476acb1265a7acb4
Michel Frechette <mifrec@hotmail.com>
2018-04-15 07:44:06 -0400

    5-16-N set the conditions of the if statement ( or )

--- page.php
+++ page.php
@@ -27,27 +27,30 @@
     ?>
 
     <?php
-    $testArray = get_pages(array(
-      'child_of' => get_the_ID();
-    ));
-    if () { ?>
+      $testArray = get_pages(array(
+        'child_of' => get_the_ID()
+      ));
+
+    if ($theParent or $testArray)
+    { ?>
+
       <div class="page-links">
         <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
         <ul class="min-list">
-          <?php
 
-            if ($theParent) {
-              $findChildrenOf = $theParent;
-            } else {
-              $findChildrenOf = get_the_ID();
-            }
+    <?php
+      if ($theParent) {
+        $findChildrenOf = $theParent;
+      } else {
+        $findChildrenOf = get_the_ID();
+      }
 
-            wp_list_pages(array(
-              'title_li' => NULL,
-              'child_of' => $findChildrenOf
-            ));
+      wp_list_pages(array(
+        'title_li' => NULL,
+        'child_of' => $findChildrenOf
+      ));
 
-          ?>
+    ?>
         </ul>
       </div>
     <?php } ?>
