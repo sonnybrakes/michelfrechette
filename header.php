bc81c672fb6b06d4f356795c220ca36feaeafc30
Michel Frechette <mifrec@hotmail.com>
2018-04-21 15:19:05 -0400

    5-18-O if the statement is true then we want to echo out a css class that will add highlight to the page we are currently on

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
               ));
             ?> -->
             <ul>
-              <li <?php if () ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+              <li <?php if () echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
