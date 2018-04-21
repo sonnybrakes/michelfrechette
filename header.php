62b863406c0b5ea4c5416e7543f2244851d47272
Michel Frechette <mifrec@hotmail.com>
2018-04-21 15:00:42 -0400

    5-18-N inside the list item drop into php and use the power of the if statement  <li <?php if () {} ?>>

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
               ));
             ?> -->
             <ul>
-              <li><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+              <li <?php if () {} ?>><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
