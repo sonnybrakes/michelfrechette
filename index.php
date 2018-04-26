8fc6c769cc663b63bb6a241bb7d6af7b535698a2
Michel Frechette <mifrec@hotmail.com>
2018-04-26 07:46:00 -0400

    6-19-S inside of the existing <a> tag add the class='btn btn--blue', this class will add a big blue button to each of our posts

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ get_header(); ?>
           </div>
           <div class="generic-content">
             <?php  the_excerpt(); ?>
-            <p><a href="<?php the_permalink(); ?>">Continue Reading &raquo;</a></p>
+            <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue Reading &raquo;</a></p>
           </div>
       <?php  }
        ?>
