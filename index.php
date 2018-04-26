81b52a0846d009494353da0839bf3657488ced8e
Michel Frechette <mifrec@hotmail.com>
2018-04-26 09:08:33 -0400

    6-19-V drop into php and call the_time() wp-function, this pulls in an un-useful time which is meaningless

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ get_header(); ?>
             <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           </div>
           <div class="metabox">
-            <p>Posted by <?php the_author_posts_link(); ?> on April 26, 2018 in News </p>
+            <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time(); ?> in News </p>
           </div>
           <div class="generic-content">
             <?php  the_excerpt(); ?>
