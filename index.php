266557f9f78548a4c256f2b4bf9105b816088b06
Michel Frechette <mifrec@hotmail.com>
2018-04-26 07:26:57 -0400

    6-19-P create yet another <div> then drop into php and call the_content() wp-function or the_excerpt(), which is what we will be calling

--- index.php
+++ index.php
@@ -23,7 +23,10 @@ get_header(); ?>
             <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           </div>
           <div class="metabox">
-            <p>Posted by Michel on April 26, 2018 in News</p>
+            <p>Posted by Michel on April 26, 2018 in News </p>
+          </div>
+          <div class="generic-content">
+            <?php  the_excerpt(); ?>
           </div>
       <?php  }
        ?>
