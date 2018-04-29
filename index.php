d2013f2a46307ca8a5a21674e4b255dbaa3525cf
Michel Frechette <mifrec@hotmail.com>
2018-04-29 08:18:30 -0400

    6-19-X replace hard code with dynamic code drop into php echo wp-function get_the_category_list(', '); and drop out of php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ get_header(); ?>
             <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           </div>
           <div class="metabox">
-            <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('F d, Y'); ?> in News </p>
+            <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('F d, Y'); ?> in <?php echo get_the_category_list(', '); ?></p>
           </div>
           <div class="generic-content">
             <?php  the_excerpt(); ?>
