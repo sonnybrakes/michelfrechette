9b5450fc65be5d3c575b5e0b3eea1b559763ccb7
Michel Frechette <mifrec@hotmail.com>
2018-05-02 16:28:15 -0400

    6-21-P delete hard code

--- archive.php
+++ archive.php
@@ -10,7 +10,7 @@ get_header(); ?>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_archive_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>Keep up with our latest news.</p>
+          <p></p>
         </div>
     </div>
 </div>
