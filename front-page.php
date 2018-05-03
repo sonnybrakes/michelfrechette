2091e86706c55a01cd8d62e2ea4318575601ffd3
Michel Frechette <mifrec@hotmail.com>
2018-05-03 13:18:23 -0400

    6-22-R delete hard code and then drop into php and call the_time('d'); function, c/w 'd' argument for the date

--- front-page.php
+++ front-page.php
@@ -55,7 +55,7 @@
           <div class="event-summary">
             <a class="event-summary__date event-summary__date--beige t-center" href="#">
               <span class="event-summary__month"><?php the_time('M'); ?></span>
-              <span class="event-summary__day">20</span>
+              <span class="event-summary__day"><?php the_time('d'); ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
