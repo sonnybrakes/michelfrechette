345e9bbcec129c3028df72ccda008640f2bf554f
Michel Frechette <mifrec@hotmail.com>
2018-05-03 13:21:20 -0400

    6-22-T delete hard code and then drop into php and call the_title(); function

--- front-page.php
+++ front-page.php
@@ -58,7 +58,7 @@
               <span class="event-summary__day"><?php the_time('d'); ?></span>
             </a>
             <div class="event-summary__content">
-              <h5 class="event-summary__title headline headline--tiny"><a href="#">We Were Voted Best School</a></h5>
+              <h5 class="event-summary__title headline headline--tiny"><a href="#"><?php the_title(); ?></a></h5>
               <p>For the 100th year in a row we are voted #1. <a href="#" class="nu gray">Read more</a></p>
             </div>
           </div>
